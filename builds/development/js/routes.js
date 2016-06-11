7f2f3dd8c0711889ebfb6a426dda34dc5ae58601
Brett East <brett@sproutback.com>
2016-06-11 13:03:15 -0700

    Changed local dir

--- builds/development/js/routes.js
+++ builds/development/js/routes.js
@@ -5,12 +5,17 @@ myFunctionsApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider
     
     .when('/', {
-        templateUrl: '/pages/home.html',
+        templateUrl: '/pages/numbers/toExponential.html',
         controller: 'mainController'
     })
     
     .when('/toExponential', {
         templateUrl: '/pages/numbers/toExponential.html',
+        controller: 'mainController'
+    })
+    
+    .when('/days', {
+        templateUrl: '/pages/home.html',
         controller: 'mainController'
     })
     
